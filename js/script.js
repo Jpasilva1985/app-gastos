2110190931fd6bc608aaf8c9b9379573fc9fe66e
Replit user <>
2023-07-24 15:33:57 +0000

    continuando js

--- js/script.js
+++ js/script.js
@@ -7,3 +7,61 @@ const btnNew = document.querySelector("#btnNew");
 const incomes = document.querySelector(".incomes");
 const expenses = document.querySelector(".expenses");
 const total = document.querySelector(".total");
+
+let items;
+
+function deleteItem(index) {
+  items.splice(index, 1);
+  setItensBD();
+  loadItens();
+}
+
+function insertItem(item, index) {
+  let tr = document.createElement("tr");
+
+  tr.innerHTML =
+  <td>${item.desc}</td>
+  <td>R$ ${item.amount}</td>
+  <td class="columnType">${
+   item.type === "Entrada"
+    ? '<i class="bx bxs-chevron-up-circle"></i>'
+    : '<i class="bx bxs-chevron-down-circle"></i>'
+  }</td>
+    <td class="columnAction">
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+    </td>
+    ;
+
+    tbody.appendChild(tr);
+}
+
+function loadItens(){
+  items = getItensBD();
+  tbody.innerHTML = "";
+  items.forEach((item, index) => {
+    insertItem(item, index);
+  });
+
+    getTotals();
+}
+
+function getTotals() {
+  const amountIncomes = items
+    .filter((item) =>item.type === "Entrada")
+    .map((transaction) => Number(transaction.amount));
+    
+  const amountIncomes = items
+    .filter((item) =>item.type === "Saida")
+    .map((transaction) => Number(transaction.amount));
+
+    const totalIncomes = amountIncomes
+    .reduce((acc, cur) => acc + cur, 0 )
+    .toFixed(2);
+}    
+    
+
+const getItensBD = () => JSON.parse(localStorage.getItem(db_items)) ?? [];
+const setItensBD = () =>
+  localStorage.setItem("db_items", JSON.stringify(items));
+
+loadItens();
